ce2518bf37e0b2f77058ba3af7923de4ddb68509
tranquansp <tranquansp@gmail.com>
2014-08-07 16:24:19 +0700

    [Aug 7 2014] commit app.js + load data to detail page

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+var Step = require('step');
 var express = require('express')
   , http = require('http');
 var mongodb = require("mongodb");
 var fs = require('fs');
-var NUM_PAGEFILMS = 10;
 
+var NUM_PAGEFILMS = 10;
 var host = "localhost";
 var HTTP_STRING = 'http://';
 var SYM_COLON = ":";
@@ -13,6 +14,14 @@ var SYM_SEPARATE = "/";
 var FILE_STYLE_CSS =  'style.css';
 var port = 1234;
 
+var FILM_ID = 'id';
+var FILM_NAME = 'name';
+var FILM_SUMMARY = 'summary';
+var FILM_DATE = 'date';
+var FILM_INFORMATION = 'information';
+var FILM_DIRECTOR = 'director';
+var FILM_ACTOR = 'actors';
+var FILM_IMAGE = 'image';
 
 var strHeader = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">';
 strHeader += '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /><meta name="description" content="Simple Template #3 from simpletemplates.org" /><meta name="keywords" content="simple #3, template, simpletemplates.org" />';
@@ -55,8 +64,8 @@ app.get("/",function(request,response){
 					//console.log(" total item : " + items.length);
 					
 					for(var i=0;i<items.length;i++){
-						resultStr += '<div id="content"><h2>';
-						resultStr += items[i].name;
+						resultStr += '<div id="content"><h2><a href="/new/' + items[i].id + '">';
+						resultStr += items[i].name + "</a>";
 						resultStr += '</h2><img style="margin: 0 20px 20px 0; float: left; margin: 0 20px 0 0; width:180px; height: 250px;" src="';
 						resultStr += PUBLIC_DIR + IMAGE_DATA_DIR + SYM_SEPARATE + items[i].image;
 						resultStr += '" alt="image" /><p><b>Information:</b></p>';
@@ -71,7 +80,7 @@ app.get("/",function(request,response){
 	});
 	
 app.get("/new",function(request,response){
-		var server = new mongodb.Server('localhost',27017,{auto_reconnect:true},10);
+	var server = new mongodb.Server('localhost',27017,{auto_reconnect:true},10);
 	var db = new mongodb.Db("mydb",server,{w:1});
 		db.open(function(err,db){
 			if(err){
@@ -85,8 +94,8 @@ app.get("/new",function(request,response){
 					//console.log(" total item : " + items.length);
 					
 					for(var i=0;i<items.length;i++){
-						resultStr += '<div id="content"><h2>';
-						resultStr += items[i].name;
+						resultStr += '<div id="content"><h2><a href="/new/' + items[i].id + '">';
+						resultStr += items[i].name + "</a>";
 						resultStr += '</h2><img style="margin: 0 20px 20px 0; float: left; margin: 0 20px 0 0; width:180px; height: 250px;" src="';
 						resultStr += PUBLIC_DIR + IMAGE_DATA_DIR + SYM_SEPARATE + items[i].image;
 						resultStr += '" alt="image" /><p><b>Information:</b></p>';
@@ -101,10 +110,40 @@ app.get("/new",function(request,response){
 	});
 	
 app.get("/new/:id",function(request,response){
-		response.send(request.params.id);
-		//response.send(str);
-	});
+	var server = new mongodb.Server('localhost',27017,{auto_reconnect:true},10);
+	var db = new mongodb.Db("mydb",server,{w:1});
+	db.open(function(err,db){
+			if(err){
+				console.log("err occured.");
+			}
+		var responeStr;
+		db.collection('cl_currFilms',function(err,collection){		
+			//console.log(request.params.id);
+			if(request.params.id != null)
+			{
+				var value = parseInt(request.params.id);
+				var query = {};
+				query[FILM_ID] = value;
 
+				collection.findOne(query,function(err,result){
+					
+				var resultRes = '<div id="content"><img style="margin: 0 20px 20px 0; float: left; margin: 0 20px 0 0; width:250px; height: 320px;" src="';	
+				resultRes += PUBLIC_DIR + IMAGE_DATA_DIR + SYM_SEPARATE + result.image;
+				resultRes += '" alt="image" /><h2><b>';
+				resultRes += result.name;
+				resultRes += '</b></h2><p><b>Director:</b>DIRECTOR</p><p><b>Cast:</b>Cast</p><p><b>Release Date:</b>';
+				resultRes += result.date;
+				resultRes +=  '</p><p><b>Running time:</b>Time</p><p><b>Category:</b>Cate</p><p><b>Information:</b>';
+				resultRes += result.information;
+				resultRes +=  '</p></div>';
+				if (err) throw err;
+				response.send(strHeader + resultRes + strFooter);	
+				});	
+			}	
+		});
+	});
+});
+	
 // if request is not good
 app.get('*', function(req, res){
   res.send('<div style="margin-top:50px;text-align:center;"><h1>Page not found<h1><h2><a href="/">Quay về Trang Chủ</a></h2></div>', 404);
