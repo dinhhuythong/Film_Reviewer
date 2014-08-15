527f81a58d11c4e3379adb2e533957e7afbedea4
tranquansp <tranquansp@gmail.com>
2014-08-15 20:41:53 +0700

    fn

---
+++ node_modules/film-node/lib/pool/config.js
@@ -0,0 +1,10 @@
+var config = {
+    MongoInstances :[
+        {name:'default',host:'127.0.0.1',port:27017},
+        {name:'default1',host:'127.0.0.1',port:27017},
+        {name:'default2',host:'127.0.0.1',port:27017},
+        {name:'defaul3',host:'127.0.0.1',port:27017}
+    ]
+};
+
+module.exports = config;
